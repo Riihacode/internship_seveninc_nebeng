9207e79f54ce1d9ad518e0ba1f9d3795952654b8
Riihacode <riihacode@gmail.com>
2025-10-18 17:30:59 +0700

    [BACKEND] Added RegencyController source code - just forgot about it lol

--- backend_api/app/Http/Controllers/RegencyController.php
+++ backend_api/app/Http/Controllers/RegencyController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\RegencyService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class RegencyController extends Controller
 {
     //
+    protected $regencyService;
+
+    public function __construct(RegencyService $regencyService) {
+        $this->regencyService = $regencyService;
+    }
+
+    public function index() {
+        $data = $this->regencyService->listRegencies();
+        return response()->json(['data' => $data], 200);
+    }
+
+    public function show($id) {
+        try {
+            $data = $this->regencyService->getRegency($id);
+            return response()->json(['data' => $data], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['message' => $e], 404);
+        }
+    }
+
+    public function getByProvince($provinceId) {
+        $data = $this->regencyService->getRegency($provinceId);
+        return response()->json(['data' => $data], 200);
+    }
 }
