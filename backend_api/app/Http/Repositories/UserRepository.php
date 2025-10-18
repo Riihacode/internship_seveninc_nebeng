1e1d048c330c5c749706f7c26490be597002497b
Riihacode <riihacode@gmail.com>
2025-10-18 11:42:35 +0700

    [BACKEND] Fixing UserRepository to use Dependency Injection with __construct function

--- backend_api/app/Http/Repositories/UserRepository.php
+++ backend_api/app/Http/Repositories/UserRepository.php
@@ -7,22 +7,45 @@ use Illuminate\Support\Facades\Hash;
 
 class UserRepository{
 
-    public function createUser(array $data){
-        return User::create([
-            'name' => $data['name'],
-            'username' => $data['username'],
-            'email' => $data['email'],
-            // 'role' => $data['role'],
-            'password' => Hash::make($data['password']),
-            'user_type' => $data['user_type'],
+    protected $model;
+
+    public function __construct(User $user) {
+        $this->model = $user;
+    }
+
+    // public function createUser(array $data){
+    //     return User::create([
+    //         'name' => $data['name'],
+    //         'username' => $data['username'],
+    //         'email' => $data['email'],
+    //         // 'role' => $data['role'],
+    //         'password' => Hash::make($data['password']),
+    //         'user_type' => $data['user_type'],
+    //     ]);
+    // }
+    public function createUser(array $data) {
+        return $this->model->create([
+            'name'      => $data['name'],
+            'username'  => $data ['username'],
+            'email'     => $data['email'],
+            'password'  => $data['password'],
+            'user_type' => $data['uset_type'],
         ]);
     }
 
-    public function findByEmailOrUsername(string $login){
-        return User::where('email', $login)
+    // public function findByEmailOrUsername(string $login){
+    //     return User::where('email', $login)
+    //         ->orWhere('username', $login)
+    //         ->first();
+    // }
+
+    public function findByEmailOrUsername(string $login) {
+        return $this->model
+            ->where('email', $login)
             ->orWhere('username', $login)
             ->first();
     }
+
     public function updateUser(User $user, array $data){
         return $user->update($data);
     }
@@ -31,11 +54,19 @@ class UserRepository{
         return $user->delete();
     }
 
-    public function getAllUser(){
-        return User::all();
+    // public function getAllUser(){
+    //     return User::all();
+    // }
+
+    public function getAllUser() {
+        return $this->model->all();
     }
 
-    public function findByIdUser($id){
-        return User::findOrFail($id);
+    // public function findByIdUser($id){
+    //     return User::findOrFail($id);
+    // }
+
+    public function findByIdUser($id) {
+        return $this->model->findOrFail($id);
     }
 }
