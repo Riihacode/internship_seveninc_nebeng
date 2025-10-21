7654ca4179a632a388a1c8c540be8706cd122ea8
Riihacode <riihacode@gmail.com>
2025-10-21 19:29:39 +0700

    [BACKEND] Terminal Models change strin into float for latitude & longitude

--- backend_api/app/Models/Terminal.php
+++ backend_api/app/Models/Terminal.php
@@ -123,7 +123,9 @@ class Terminal extends Model
      * Bisa dipakai jika nanti longitude dan latitude mau disimpan sebagai float
      */
     protected $casts = [
-        'longitude' => 'string',
-        'latitude' => 'string',
+        // 'longitude' => 'string',
+        // 'latitude' => 'string',
+        'latitude'  => 'float',
+        'longitude' => 'float',
     ];
 }
